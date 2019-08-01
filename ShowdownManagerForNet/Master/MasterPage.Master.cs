e24b685c258a45b8ac7b1947ac93ddc0716af9b5
scoop99 <52905629+scoop99@users.noreply.github.com>
2019-08-01 14:41:43 +0900

    Add files via upload

---
+++ ShowdownManagerForNet/Master/MasterPage.Master.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ShowdownManagerForNet.Master
+{
+    public partial class MasterPage : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string strUrl = Request.Url.AbsolutePath;
+
+            if (strUrl != "/Login.aspx")
+            {
+                if (HttpContext.Current.Session["id"] == null)
+                {
+                    string error = @"
+                            <script type='text/javascript'>
+                                     window.alert('로그인 정보가 없습니다.');
+                                       location.href='Login.aspx';
+                            </script>
+                        ";
+                    Response.Write(error);
+                }
+                else
+                {
+                    if (HttpContext.Current.Session["id"].ToString() != "admin")
+                    {
+                        string error = @"
+                            <script type='text/javascript'>
+                                     window.alert('로그인 정보가 없습니다.');
+                                       location.href='Login.aspx';
+                            </script>
+                        ";
+                        Response.Write(error);
+                    }
+                }
+            }
+        }
+    }
+}
+   
